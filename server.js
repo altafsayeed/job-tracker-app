15b0f7593ff312c7c27b18234760cef3d685b151
altafsayeed <altafsayeed19@yahoo.com>
2022-11-20 02:50:31 -0800

    routes and controllers

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connect.js";
+import authRouter from "./routes/authRoutes.js";
+import jobsRouter from "./routes/jobsRoutes.js";
+
 const app = express();
 dotenv.config();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("WELCOME!");
 });
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
