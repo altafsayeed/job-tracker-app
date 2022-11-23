221f49b9a6a29097d40212515163899202d06a69
altafsayeed <altafsayeed19@yahoo.com>
2022-11-23 00:39:54 -0800

    navbar and sidebars

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,24 @@
 import Wrapper from "../assets/wrappers/BigSidebar";
+import { useAppContext } from "../context/appContext";
+import NavLinks from "./NavLinks";
+import Logo from "./Logo";
 
 const BigSidebar = () => {
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div>BigSidebar</div>
+      <div
+        className={
+          !showSidebar ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggleSidebar} />
+        </div>
+      </div>
     </Wrapper>
   );
 };
